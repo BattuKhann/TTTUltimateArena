b5b908491b11a60902cc56c2387b6deaa0704209
Batu Khan <67027257+BattuKhann@users.noreply.github.com>
2026-06-22 21:40:29 -0400

    Optimizes Game Representation
    Removes use of expensive 128 bit shifts
    Uses separate variables for global and draw boards
    Uses no extra space

--- src/core/types.hpp
+++ src/core/types.hpp
@@ -1,10 +1,10 @@
 #include <stdint.h>
 #include "../common/constants.hpp"
 
-#define GLOBAL_BOARD_MASK (unsigned __int128)0b0000000111111111 << 112
-
 struct BoardState {
-    unsigned __int128 players[2]; //Boards use 9 MSB for Global Board
-    uint8_t currBoard; //0xFF if all boards available
-    uint8_t currPlayer;
+    unsigned __int128 players[2]; //Lowest 81 bits for whole board
+    uint16_t globalBoards[2];
+    uint16_t drawBoard;
+    uint16_t currBoard; //One-Hot, All open boards hot
+    uint16_t currPlayer;
 };
